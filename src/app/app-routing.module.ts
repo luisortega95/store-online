e3f80bf9fa0bf6b089d7d4aede1c4e0926fda73c
Luis Ortega Mantilla <luis.ortega@sofka.local>
2022-08-26 22:21:33 -0500

    Imports, instalations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductsComponent} from "./page/products/products.component";
+import {AboutComponent} from "./page/about/about.component";
+import {CartComponent} from "./page/cart/cart.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: 'products', component: ProductsComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MenuComponent} from './components/menu/menu.component';
+import {ProductsComponent} from './page/products/products.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AboutComponent} from './page/about/about.component';
+import {CartComponent} from './page/cart/cart.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {HttpClientModule} from "@angular/common/http";
+import {NgxPaginationModule} from "ngx-pagination";
+import {ToastrModule} from "ngx-toastr";
+import {MatGridListModule} from "@angular/material/grid-list";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ProductsComponent,
+    AboutComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    HttpClientModule,
+    NgxPaginationModule,
+    ToastrModule.forRoot({
+      timeOut: 1500,
+      positionClass: 'toast-top-right',
+      preventDuplicates: false,
+    }),
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
